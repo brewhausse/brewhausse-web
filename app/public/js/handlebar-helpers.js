f313913bb8ad6e32fe7273a7fa952b29c443572f
Marshall Hauser <mhauser@paradym.com>
2017-09-18 17:54:17 -0400

    * Splitting project html pages out into their own folder
    * Updated the project page id's for routing.

--- app/public/js/handlebar-helpers.js
+++ app/public/js/handlebar-helpers.js
@@ -1,36 +1,22 @@
-var _  = require("underscore");
-var register = function(Handlebars) {
-    var helpers = {
-        everyNth: function(context, every, options) {
-            var fn = options.fn, inverse = options.inverse;
-            var ret = "";
-            if(context && context.length > 0) {
-              for(var i=0, j=context.length; i<j; i++) {
+var _ = require("underscore");
+var helpers = {
+    everyNth: function (context, every, options) {
+        var fn = options.fn, inverse = options.inverse;
+        var ret = "";
+        if (context && context.length > 0) {
+            for (var i = 0, j = context.length; i < j; i++) {
                 var modZero = i % every === 0;
                 ret = ret + fn(_.extend({}, context[i], {
-                  isModZero: modZero,
-                  isModZeroNotFirst: modZero && i > 0,
-                  isLast: i === context.length - 1,
-                  json: every
+                    isModZero: modZero,
+                    isModZeroNotFirst: modZero && i > 0,
+                    isLast: i === context.length - 1
                 }));
-              }
-            } else {
-              ret = inverse(this);
             }
-            return ret;
-          }
-    };
-
-    if (Handlebars && typeof Handlebars.registerHelper == "function") {
-        for (var prop in helpers) {
-            Handlebars.registerHelper(prop, helpers[prop]);
+        } else {
+            ret = inverse(this);
         }
-    } else {
-        // just return the helpers object if we can't register them
-        return helpers;
+        return ret;
     }
-
 };
 
-module.exports.register = register;
-module.exports.helpers = register(null);
+module.exports = helpers;
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,38 +1,53 @@
+var fs = require('fs');
+var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 
 var data = [
     {
-        id: 1,
-        imageUrl: "http://placehold.it/150x150",
+        id: "timelapse",
+        imageUrl: "http://placehold.it/350x350",
         title: "Eclipse timelapse",
-        short: "Raspberry Pi camera project used to take a timelapse of the 2017 eclipse.",
-        content: "Content goes here..."
+        short: "Raspberry Pi camera project used to take a timelapse of the 2017 eclipse."
     },
     {
-        id: 2,
-        imageUrl: "http://placehold.it/150x150",
+        id: "raspi-cam",
+        imageUrl: "http://placehold.it/350x350",
         title: "Raspi-cam project",
-        short: "Creating an easy to use web app to setup timelapses.",
-        content: "Content goes here..."
+        short: "Creating an easy to use web app to setup timelapses."
     },
     {
-        id: 3,
-        imageUrl: "http://placehold.it/150x150",
+        id: "ci",
+        imageUrl: "http://placehold.it/350x350",
         title: "Continuous Integration",
-        short: "TeamCity to the rescue!",
-        content: "Content goes here..."
+        short: "TeamCity to the rescue!"
     }
 ];
 
 router.get('/', function(req, res, next) {
-    console.log("/ route");
     res.render('projects', { title: 'Projects', projects: data});
 });
 
 router.get('/:id', function(req, res, next) {
-    var selectedProject = data[req.params.id - 1];    
-    res.render('project', { project: selectedProject });
+
+    var ret = {};
+
+    var selectedProject = _.find(data, function(item){
+        return item.id === req.params.id;
+    });
+
+    fs.readFile("./projects/" + req.params.id + ".html", 'utf8', function(err, html){
+        if (err) {
+            throw err;
+        }
+
+        _.extend(ret, selectedProject, {
+            html: html
+        });
+
+    });
+        
+    res.render('project', { project: ret });
 });
 
 module.exports = router;
